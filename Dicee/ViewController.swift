66fece2da9374af877a65db332e0eca7bf255c1f
Shiyao <liushiyao2014@gmail.com>
2017-08-24 23:39:29 -0500

    Add component in controller

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
